656bcf20efcca6a36d5eb28bdd15bdbe186a5d69
aysegulyilmaz5 <83874900+aysegulyilmaz5@users.noreply.github.com>
2022-02-07 01:05:16 +0300

    string-literal-interpolitan.dart

---
+++ string-literal-interpolitan.dart
@@ -0,0 +1,18 @@
+main(List<String> args){
+  String name = "aysegul";
+  String surName = "yilmaz";
+  var course = 'Dart\'s Uses';
+  String courseinformation = " We will learn dart and flutter";
+  
+  print(name + " " + surName);
+  print("$name $surName");
+  print(" is in my surname $surName character number: " + surName.length.toString());
+  print("Character number ${surName.length}");
+  
+  double area =10;
+  double height = 12;
+  
+  print("Area ${area.toInt()} and height ${height.toInt()} Area of the rectangle ${area.toInt()*height.toInt()}");
+  
+  
+}
